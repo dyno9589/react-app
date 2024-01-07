c96359356a020b24dc8b35cc20c8cfaf0d3403ff
dineshkjid <dineshkumarprajapati.ittcoer@kjei.edu.in>
2024-01-07 16:08:15 +0530

    todo app

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Footer = () => {
+  // let footerStyle={
+  //   position:"relative",
+  //   top:"40vh",
+  //   width:"100%"
+  // }
+  return (
+    <footer className="bg-dark text-light py-3" /*style={footerStyle}*/ >
+      <p className="text-center">
+      Copyright &copy; surpex.com
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
